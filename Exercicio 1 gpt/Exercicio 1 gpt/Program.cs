19e80930a2fd163f22855b924ee9a5ad260aba39
fantindev <fantin.dev@gmail.com>
2026-07-13 16:39:52 -0300

    Adicionando exercicios

---
+++ Exercicio 1 gpt/Exercicio 1 gpt/Program.cs
@@ -0,0 +1,42 @@
+﻿using Exercicio_1_gpt;
+using System;
+
+namespace Exercicio_1_gpt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Quantos assentos serão comprados? ");
+            int Acentos = int.Parse(Console.ReadLine());
+
+
+            Pessoa[] vect = new Pessoa[10];
+
+            
+            for(int i = 0; i < Acentos; i++) 
+            {
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Idade: ");
+                int age = int.Parse(Console.ReadLine());
+                Console.Write("Acento: ");
+                int assento = int.Parse(Console.ReadLine());
+                vect[assento] = new Pessoa(name, age);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Acentos ocupados:");
+
+            for (int i = 0;i < 10; i++)
+            {
+                if(vect[i] != null) { 
+                Console.WriteLine($"{i}: {vect[i]}");
+                 }
+
+            }
+        }
+    }
+}
+
---
+++ Exercicio2GPT/Exercicio2GPT/Produto.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio2GPT
+{
+    class Produto
+    {
+
+        public string Name { get; set; }
+
+        public double Value { get; set; }
+
+
+        public Produto(string name, double value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+
+        public override string ToString()
+        {
+            return Name + ", R$" + Value;
+
+
+        }
+    }
+}
