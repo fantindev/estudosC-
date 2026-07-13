1d0a7b31a1e1b8d9c8aaa3744bf0ce369f26de91
fantindev <fantin.dev@gmail.com>
2026-07-13 16:09:46 -0300

    Adiciona exercicios iniciais em C#

---
+++ Exercicio1-Produto(controle de estoque)/Exercicio1-Produto(controle de estoque)/Program.cs
@@ -0,0 +1,57 @@
+﻿using Exercicio1_Produto_controle_de_estoque_;
+using System;
+
+namespace MeuPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto prod;
+
+            Console.Write("Codigo do produto: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preco do produto: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.Write("Havera estoque inicial(S/N)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 'S' || resp == 's')
+            {
+                Console.Write("Insira o valor de estoque inicial: ");
+                int quantidadeEmEstoque = int.Parse(Console.ReadLine());
+                prod = new Produto(codigo, nome, preco, quantidadeEmEstoque);
+            }
+            else
+            {
+                prod = new Produto(codigo, nome, preco);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: ");
+            Console.WriteLine(prod);
+
+            Console.WriteLine();
+            Console.Write("Insira uma quantidade para adicionar ao estoque: ");
+            int quantia = int.Parse(Console.ReadLine());
+            prod.EntradaEstoque(quantia);
+            Console.WriteLine("Dados do produto:");
+            Console.WriteLine(prod);
+
+            Console.WriteLine();
+            Console.Write("Insira uma quantiade para remover do estoque: ");
+            quantia = int.Parse(Console.ReadLine());
+            if (quantia > prod.QuantidadeEmEstoque)
+            {
+                Console.WriteLine("Estoque insuficiente, operação cancelada");
+            }
+            else
+            {
+                prod.SaidaEstoque(quantia);
+                Console.WriteLine("Dados do produto:");
+                Console.WriteLine(prod);
+
+            }
+        }
+    }
+}
---
+++ Exercicio2 - Funcionarios(folha de pagamento)/Exercicio2 - Funcionarios(folha de pagamento)/Funcionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercicio2___Funcionarios_folha_de_pagamento_
+{
+    class Funcionario
+    {
+        public int Matricula { get; set; }
+
+        public string Nome { get; set; }
+
+        public double SalarioBase { get; set; }
+
+        public int PercAumento { get; set; }
+
+        public int PercDesc { get; set; }
+
+        public Funcionario(int matricula, string nome)
+        {
+            Matricula = matricula;
+            Nome = nome;
+        }
+
+
+        public Funcionario (int matricula, string nome, double salarioBase): this (matricula, nome)
+        {
+            SalarioBase = salarioBase;
+        }
+
+        public void AumentoSalario(int percAumento)
+        {
+            SalarioBase += (SalarioBase * percAumento / 100);
+        }
+
+        public void DescontoSalarial(int percDesc)
+        {
+            SalarioBase = SalarioBase - (SalarioBase * percDesc / 100) -50.0 ;
+        }
+
+        public override string ToString()
+        {
+            return "Matricula: "
+                + Matricula
+                + ", Nome: "
+                + Nome
+                + ", Salario: "
+                + SalarioBase.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
